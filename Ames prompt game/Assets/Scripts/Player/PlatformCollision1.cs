c0d933d71bbdb80373f5ce9802024003b8638ce1
holovanist <jonsonlogan37@gmail.com>
2025-03-21 13:56:17 -0700

    platform movement broken

--- Ames prompt game/Assets/Scripts/Player/PlatformCollision1.cs
+++ Ames prompt game/Assets/Scripts/Player/PlatformCollision1.cs
@@ -4,19 +4,27 @@ public class PlatformCollision1 : MonoBehaviour
 {
     public string playerTag = "Player";
     public Transform platform;
+    public Transform parrent;
+
+    private void Start()
+    {
+        parrent = GameObject.FindGameObjectWithTag("Player").transform;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag.Equals(playerTag))
+        if (other.gameObject.CompareTag(playerTag))
         {
-            other.gameObject.transform.parent = platform;
+            parrent.transform.SetParent(platform);
+            //other.gameObject.transform.parent = platform;
         }
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.tag.Equals (playerTag))
+        if (other.gameObject.CompareTag(playerTag))
         {
-            other.gameObject.transform.parent = null;
+            parrent.transform.SetParent(null);
+            //other.gameObject.transform.parent = null;
         }
     }
 }
