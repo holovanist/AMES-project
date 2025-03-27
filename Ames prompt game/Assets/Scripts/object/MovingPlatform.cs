88fc54affbab4ca8073336adeb33bc8f446acd2f
holovanist <jonsonlogan37@gmail.com>
2025-03-27 13:57:03 -0700

    worked on moving platform

--- Ames prompt game/Assets/Scripts/object/MovingPlatform.cs
+++ Ames prompt game/Assets/Scripts/object/MovingPlatform.cs
@@ -1,16 +1,105 @@
+using NewMovment;
 using System.Collections;
 using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    public GameObject pointA;
-    public GameObject pointB;
+    //public GameObject pointA;
+    //public GameObject pointB;
     public float speed = 10f;
     public float delay = 1f;
-    public GameObject platform;
+    public Transform parrent;
+    public string playerTag = "playerCap";
+    //public GameObject platform;
 
-    private Vector3 targetPosition;
-    void Start()
+    //private Vector3 targetPosition;
+
+    public Transform startPoint, endPoint;
+
+    private Transform destinationTarget, departTarget;
+
+    private float startTime;
+
+    private float journeyLength;
+
+    bool isWaiting;
+
+    private void Start()
+    {
+        departTarget = startPoint;
+        destinationTarget = endPoint;
+        parrent = GameObject.FindGameObjectWithTag("Player").transform;
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(departTarget.position, destinationTarget.position);
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+
+    private void Move()
+    {
+        if(!isWaiting)
+        {
+            if(Vector3.Distance(transform.position, destinationTarget.position) > 0.01f)
+            {
+                float distCovered = (Time.time - startTime) * speed;
+
+                float fractionOfJourney = distCovered / journeyLength;
+
+                transform.position = Vector3.Lerp(departTarget.position, destinationTarget.position, fractionOfJourney);
+            }
+            else
+            {
+                isWaiting = true;
+                StartCoroutine(changeDelay());
+            }
+        }
+    }
+
+    private void ChangeDestination()
+    {
+         if(departTarget == endPoint && destinationTarget == startPoint)
+        {
+            departTarget = startPoint;
+            destinationTarget = endPoint;
+        }
+         else
+        {
+            departTarget = endPoint;
+            destinationTarget = startPoint;
+        }
+    }
+
+    IEnumerator changeDelay()
+    {
+        yield return new WaitForSeconds(delay);
+        ChangeDestination();
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(departTarget.position, destinationTarget.position );
+        isWaiting = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(playerTag))
+        {
+            parrent.transform.SetParent(transform);
+            //other.gameObject.transform.parent = platform;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag(playerTag))
+        {
+            parrent.transform.SetParent(null);
+            //other.gameObject.transform.parent = null;
+        }
+    }
+
+
+    /*void Start()
     {
         platform.transform.position = pointA.transform.position;
         targetPosition = pointB.transform.position;
@@ -36,5 +125,5 @@ public class MovingPlatform : MonoBehaviour
 
             yield return new WaitForSeconds(delay);
         }
-    }
+    }*/
 }
