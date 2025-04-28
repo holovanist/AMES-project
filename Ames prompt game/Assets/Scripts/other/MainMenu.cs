b130259a83fa9bbbca1765b956b9383bdeaba072
Devin <mcmanusdevin2008@gmail.com>
2025-04-28 12:04:39 -0700

    Fixing main menu scenes

--- Ames prompt game/Assets/Scripts/other/MainMenu.cs
+++ Ames prompt game/Assets/Scripts/other/MainMenu.cs
@@ -6,21 +6,14 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField]
-    string levelToLoad = "Level 1";
-    public string levelToLoad2 = "Level 1 Hard";
-    public string Select = "DifficultySelect";
+    private string levelToLoad = "Level 1";
     public string mainMenu = "Start Menu";
     public string HowToPlay = "HowToPlay";
     public string Controls1 = "Controls";
     
-    public void StartEasyGame()
+    public void StartGame()
     {
         SceneManager.LoadScene(levelToLoad);
-        Time.timeScale = 1.0f;
-    }
-    public void StartHardGame()
-    {
-        SceneManager.LoadScene(levelToLoad2);
         Time.timeScale = 1.0f;
     }
     public void QuitGame()
@@ -39,9 +32,4 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(Controls1);
     }
-    public void DifficultySelect()
-    {
-        SceneManager.LoadScene(Select);
-    }
-    
 }
